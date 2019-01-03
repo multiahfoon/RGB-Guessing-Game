37363c1f84268e01afeaa7922af573ecde446d04
multi <multiahfoon@gmail.com>
2019-01-03 11:45:17 +1100

    logic add for when the correct or incorrect color is picked

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', function(){
-
-    const rgbColorGenerator = function(){
+    function rgbColorGenerator(){
         const generateNum = [];
         let rgbString;
         for (var i = 1; i <= 3; i++){
@@ -19,7 +18,19 @@ document.addEventListener('DOMContentLoaded', function(){
         colorDisplay[i].style.backgroundColor = rgbColorGenerator();
     }
 
-    const secretColor = rgbDisplay.innerHTML = colorDisplay[(Math.floor(Math.random() * 6))].style.backgroundColor;
-
-
+    const secretColor = rgbDisplay.innerHTML = colorDisplay[(Math.floor(Math.random() * 6) + 1)].style.backgroundColor;
+    const message = document.querySelector('#message');
+    const header = document.querySelector('header');
+    for(var i = 0; i < colorDisplay.length; i++){
+        colorDisplay[i].addEventListener('click', function(){
+            console.log(this.style.backgroundColor);
+            if(this.style.backgroundColor === secretColor){
+                console.log("you picked the secret color");
+                message.textContent = "Correct!";
+                header.style.backgroundColor = this.style.backgroundColor;
+            }else{
+                message.textContent = "Try again";
+            }
+        });
+    }
 });
