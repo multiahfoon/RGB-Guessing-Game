a916446a35847d3ab628932d2c05b964f4dad06f
multi <multiahfoon@gmail.com>
2019-01-04 11:13:12 +1100

    logic added to new colors button  and code has been refactored

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,10 @@
 document.addEventListener('DOMContentLoaded', function(){
+    const rgbDisplay = document.querySelector('#rgbDisplay');    
+    const colorDisplay = document.querySelectorAll('main > .container > button');
+    const message = document.querySelector('#message');
+    const header = document.querySelector('header');
+    const newColors = document.querySelector('#newColors');
+
     function rgbColorGenerator(){
         const generateNum = [];
         let rgbString;
@@ -11,20 +17,22 @@ document.addEventListener('DOMContentLoaded', function(){
         return rgbString;
     };
 
-    const rgbDisplay = document.querySelector('#rgbDisplay');    
-    const colorDisplay = document.querySelectorAll('main > .container > button');
+    const generateMainColors = (function mainColors(){
+        for(var i = 0; i < colorDisplay.length; i++){
+            colorDisplay[i].style.backgroundColor = rgbColorGenerator();
+        }
+        return mainColors;
+    })();
+    
+    const generateSecretColor = (function secretColor(){
+        rgbDisplay.textContent = colorDisplay[(Math.floor(Math.random() * 6))].style.backgroundColor;
+        return secretColor;
+    })();
 
-    for(var i = 0; i < colorDisplay.length; i++){
-        colorDisplay[i].style.backgroundColor = rgbColorGenerator();
-    }
-
-    const secretColor = rgbDisplay.innerHTML = colorDisplay[(Math.floor(Math.random() * 6))].style.backgroundColor;
-    const message = document.querySelector('#message');
-    const header = document.querySelector('header');
     for(var i = 0; i < colorDisplay.length; i++){
         colorDisplay[i].addEventListener('click', function(){
             console.log(this.style.backgroundColor);
-            if(this.style.backgroundColor === secretColor){
+            if(this.style.backgroundColor === rgbDisplay.textContent ){
                 console.log("you picked the secret color");
                 message.textContent = "Correct!";
                 header.style.backgroundColor = this.style.backgroundColor;
@@ -33,4 +41,13 @@ document.addEventListener('DOMContentLoaded', function(){
             }
         });
     }
+    
+    newColors.addEventListener('click', function(){
+        console.log("button working!");
+        generateMainColors();
+        generateSecretColor();
+        header.style.backgroundColor = "steelblue";
+        message.textContent = "";
+    });
 });
+
